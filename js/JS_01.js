69bd2dcc43f0d7a5c011630260da318947959f02
junjieruan <1259251208@qq.com>
2016-06-10 22:11:33 +0800

    add index3.2

---
+++ js/JS_01.js
@@ -0,0 +1,9 @@
+/**
+ * Created by a on 2016/6/10.
+ */
+
+window.onload=function() {
+    document.getElementById("register").addEventListener("click", function () {
+        alert("登陆成功");
+    })
+}
