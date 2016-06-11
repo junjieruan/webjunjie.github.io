fc46cfe3f7879ae4d7dcfcea3bf24c9d63d2746b
junjieruan <1259251208@qq.com>
2016-06-11 19:37:21 +0800

    add index1

--- js/JS_01.js
+++ js/JS_01.js
@@ -6,4 +6,24 @@ window.onload=function() {
     document.getElementById("register").addEventListener("click", function () {
         alert("登陆成功");
     })
+};
+
+function startTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("timetxt").innerHTML = h+":"+m+":"+s;
+    t = setTimeout(function () {
+        startTime();
+    },500)
+}
+
+function checkTime(i){
+    if(i<10){
+        i = "0"+i;
+    }
+    return i;
 }
